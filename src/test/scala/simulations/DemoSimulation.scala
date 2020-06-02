f1359cf86762c7c7b04076ed8e725335a52e53b8
Hugo <firefactoryan@gmail.com>
2020-06-02 08:47:14 -0300

    Added post request and header

--- src/test/scala/simulations/DemoSimulation.scala
+++ src/test/scala/simulations/DemoSimulation.scala
@@ -6,10 +6,28 @@ import io.gatling.http.Predef._
 class DemoSimulation extends Simulation {
   val httpProtocol = http.baseUrl(s"http://www.mocky.io")
 
-  val scn = scenario("Exemplo de request")
-    .exec(http("Saudacoes").get("/v2/5ed4e85f3300005100f7a55b"))
+  val sentHeaders = Map(
+    "content-type" -> "application/json",
+    "authorization" -> "Basic 639b36ea-aff3-1b85-618e-c696734afc6e")
+
+  val scnGet = scenario("Exemplo de request get")
+    .exec(http("Saudacoes")
+      .get("/v2/5ed4e85f3300005100f7a55b")
+      .header("Keep-Alive", "150")
+    )
+
+  val scnPost = scenario("Exemplo de request post")
+    .exec(http("Saudacoes")
+      .post("/v2/5ed4e85f3300005100f7a55b")
+      .headers(sentHeaders)
+      .body(StringBody("""{ "myContent": "myHardCodedValue" }""")).asJson
+    )
+
   setUp(
-    scn.inject(
+    scnGet.inject(
+      atOnceUsers(1)
+    ),
+    scnPost.inject(
       atOnceUsers(1)
     )
   ).protocols(httpProtocol)
